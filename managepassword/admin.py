f74c1a53e7c99b2527cc5f0c872d264331a779e5
root <root@localhost.localdomain>
2017-03-29 11:25:41 -0400

    add admin

--- managepassword/admin.py
+++ managepassword/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Manageruser,DbList
+
+class ManagerUserAdmin(admin.ModelAdmin):
+	        list_display=('uname','password','allowed_host',)
+
+class DbListAdmin(admin.ModelAdmin):
+	        list_display=('db_name','sid','connect_str',)
+
+admin.site.register(Manageruser,ManagerUserAdmin)
+admin.site.register(DbList,DbListAdmin)
 
 # Register your models here.
